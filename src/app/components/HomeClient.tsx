e84d788ceee839b019b814289e698239fcf58381
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-02-27 21:11:13 -0300

    add create notes

--- src/app/components/HomeClient.tsx
+++ src/app/components/HomeClient.tsx
@@ -3,7 +3,8 @@
 import { ChangeEvent, useState } from "react"
 import { SeachBar } from "./Seachbar"
 import { Note } from "./Note"
-import { NotebookPenIcon } from "lucide-react"
+import { ChevronLeftIcon, NotebookPenIcon } from "lucide-react"
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet"
 
 
 export function HomeClient() {
@@ -11,15 +12,24 @@ export function HomeClient() {
   const notess = [
     { id: 1, note: 'Hello, World!' },
     { id: 2, note: ' World!' },
-    { id: 3, note: 'Hello' },
-    { id: 3, note: 'Hello' },
   ]
 
   const [seachNote, setSeachNote] = useState('')
+  const [note, setNote] = useState('')
   const [notes, setNotes] = useState(notess)
+
 
   function handleSeachNote(e: ChangeEvent<HTMLInputElement>) {
     setSeachNote(e.target.value)
+  }
+
+  function handleNewNote() {
+
+    note !== '' ? setNotes((prev) => ([
+      ...prev,
+      { id: 4, note: note }
+    ])) : null
+    setNote('')
   }
 
   return (
@@ -37,9 +47,23 @@ export function HomeClient() {
 
       <div className="w-full flex absolute justify-end  bottom-0">
         <span className="w-1/2">{notes.length} Notas</span>
-        <button>
-          <NotebookPenIcon />
-        </button>
+        <Sheet>
+          <SheetTrigger>
+            <NotebookPenIcon />
+          </SheetTrigger>
+
+          <SheetContent className="w-full flex flex-col gap-5">
+            <SheetHeader className="items-start">
+              <SheetClose className="flex" onClick={handleNewNote}>
+                <ChevronLeftIcon /> Notas
+              </SheetClose>
+            </SheetHeader>
+
+            <textarea onChange={(e) => setNote(e.target.value)}
+              className="bg-transparent outline-none w-full h-full resize-none" />
+
+          </SheetContent>
+        </Sheet>
       </div>
     </section>
   )
